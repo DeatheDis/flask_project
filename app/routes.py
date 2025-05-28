fbb68342f089e83bad8da039501d1102fd0ecdb4
DeatheDis <disdos93@gmail.com>
2025-05-29 00:29:19 +0300

    Lesson 5

--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,22 @@
+from flask import render_template
 from app import app
 
 
-@app.route('/')
-def index():
-    return 'This is start page!'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
-@app.route('/info')
-def info():
-    return 'This is the informational page'
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
-@app.route('/calc/<first_number>/<second_number>')
-def calc(first_number, second_number):
-    try:
-        first_number = int(first_number)
-        second_number = int(second_number)
-    except ValueError:
-        return 'First number or second number must be digit'
-
-    result = first_number + second_number
-    return f'The sum of {first_number} and {second_number} is {result}'
 
 
-@app.route('/reverse/')
-@app.route('/reverse/<word>')
-def reverse(word=None):
-    if not word:
-        return 'Enter a word'
-    return f'The reverse of {word} is {word[::-1]}'
-
-
-@app.route('/user/<name>/<int:age>')
-def user(name, age):
-    if age <= 0:
-        return 'Your age must be more than 0'
-    return f'Hello, {name}. You are {age} years old.'
 
