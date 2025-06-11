30b8f175b9248317f9d6a1f417e0c7322d681e15
DeatheDis <disdos93@gmail.com>
2025-06-12 00:38:07 +0300

    -Обновлены шаблоны: about.html, contact.html, index.html
    -Добавлено отображение даты и времени в index.html
    
    -В базовый шаблон base.html добавлен блок bottom_info для отображения информации снизу, туда помещено отображение даты и времени
    
    -На странице about.html добавлен список из нескольких пользователей с именами и их ролями (Использован цикл for)
    
    -На странице contact.html добавлена информация менеджера по работе с клиентами (Использован вложенный словарь)
    
    -Обновлен стиль: index.css
    
    -Обновлен функционал маршрутов в routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,41 @@
 from flask import render_template, request, url_for, redirect, flash
 from app import app
 from app.forms import ContactForm
+from datetime import datetime
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    current_time = datetime.now()
+    return render_template('index.html', current_time=current_time)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template('about.html', team_members=team_members)
 
 
 @app.route('/contact', methods=['POST', 'GET'])
 def contact():
     form = ContactForm()
+    department_info = {
+        'manager': {
+            'name': 'Ирина Смирнова',
+            'position': 'Менеджер по работе с клиентами',
+            'email': 'irina@gmail.com'
+        },
+        'address': {
+            'street': 'ул. Есенина, д. Каруселина',
+            'city': 'Москва',
+            'zip': '12345'
+        }
+    }
+
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
@@ -26,7 +46,7 @@ def contact():
     if request.method == 'POST':
         flash('There were errors in the form', 'error')
 
-    return render_template('contact.html', form=form)
+    return render_template('contact.html', form=form, department_info=department_info)
 
 
 
